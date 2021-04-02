02a6cb28c366cb36e4a003a17948efdf2a300560
InSyn <Synergy95@yandex.ru>
2021-04-02 11:58:16 +0700

    1

--- src/renderer/components/final_protocols/preview.vue
+++ src/renderer/components/final_protocols/preview.vue
@@ -6,13 +6,34 @@
       backgroundColor: $vuetify.theme.themes[appTheme].cardBackgroundRGBA
     }"
   >
-    preview
+    <vue-html2pdf
+      :show-layout="false"
+      :float-layout="true"
+      :enable-download="true"
+      :preview-modal="true"
+      :paginate-elements-by-height="1400"
+      filename="hee hee"
+      :pdf-quality="2"
+      :manual-pagination="false"
+      pdf-format="a4"
+      pdf-orientation="landscape"
+      pdf-content-width="800px"
+      ref="html2Pdf"
+    >
+      <section slot="pdf-content">
+        <div>PREVIEW</div>
+      </section>
+    </vue-html2pdf>
   </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
+import VueHtml2pdf from "vue-html2pdf";
 export default {
   name: "preview",
+  components: {
+    VueHtml2pdf
+  },
   computed: {
     ...mapGetters("main", ["competition", "appTheme"])
   }
