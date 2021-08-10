a732f7d96fb8715ded798481e29f184746e38f1c
InSyn <Synergy95@yandex.ru>
2021-08-10 11:29:22 +0700

    1

--- src/renderer/components/final_protocols/preview.vue
+++ src/renderer/components/final_protocols/preview.vue
@@ -6,16 +6,66 @@
       backgroundColor: $vuetify.theme.themes[appTheme].cardBackgroundRGBA
     }"
   >
-    <section slot="pdf-content">
-      <div>PREVIEW</div>
+    <section id="pdf_to_print">
+      <div
+        slot="pdf-item"
+        class="pdf_container"
+        :style="{
+          height: `${setup.height}px`,
+          width: `${setup.width}px`,
+          padding: `${setup.padding}px`
+        }"
+        style="background-color: aliceblue; color: slategrey"
+      >
+        <div class="pdf_header">PROTOCOL HEADER</div>
+        <div class="pdf_content">
+          {{ $store.getters["main/competition"].competitors }}
+        </div>
+        <div class="pdf_footer">PDF FOOTER</div>
+      </div>
     </section>
+    <v-btn
+      @click="print_pdf()"
+      style="background-color: slategrey; color: aliceblue"
+      >Download</v-btn
+    >
   </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
+import html2pdf from "html2pdf.js";
 export default {
   name: "preview",
-  components: {},
+  data() {
+    return {
+      setup: {
+        height: 1080,
+        width: 600,
+        padding: 16
+      }
+    };
+  },
+  methods: {
+    print_pdf() {
+      try {
+        let element = document.getElementById("element-pdf_to_print-print");
+        const opt = {
+          margin: 1,
+          filename: "F://Downloads/myfile.pdf",
+          image: { type: "jpeg", quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: "in", format: "letter", orientation: "portrait" }
+        };
+        console.log(opt);
+        html2pdf()
+          .set(opt)
+          .from(element)
+          .save();
+      } catch (e) {
+        throw e;
+      }
+    }
+  },
   computed: {
     ...mapGetters("main", ["competition", "appTheme"])
   }
