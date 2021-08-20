dc7380607f627c081b2ae27d3470cd8ddfde60a9
InSyn <Synergy95@yandex.ru>
2021-08-20 16:52:33 +0700

    Update fonts.vue

--- src/renderer/components/final_protocols/fonts.vue
+++ src/renderer/components/final_protocols/fonts.vue
@@ -1,29 +1,31 @@
 <template>
   <div
-    class="d-flex flex-column justify-center align-center"
-    style="border-radius: 6px; width: 100%; height: 100%;overflow-y: auto"
+    style="border-radius: 6px; width: 100%; height: 100%; padding: 16px; display:flex;justify-content:center;align-items: center"
     :style="{
       backgroundColor: $vuetify.theme.themes[appTheme].cardBackgroundRGBA
     }"
   >
     <div
-      class="pa-2"
-      style="border-radius: 4px; border: 1px solid #c3d9ff"
+      v-for="container in get_blocks"
+      style="border-radius: 4px; border: 1px solid #c3d9ff;max-height: 100%; overflow-y: auto;padding: 4px"
       ref="container"
     >
       <div
+        v-for="(block, bi) in container"
+        :key="bi"
+        :ref="`block_${bi}`"
         @click="
           () => {
             blocks.push('block');
           }
         "
-        v-for="(block, bi) in get_blocks"
-        :key="bi"
+        style="padding: 4px 2px"
       >
         {{ `${block}` }}
       </div>
     </div>
-    {{ height }}
+    {{ height_container }}
+    <div>{{ height_blocks }}</div>
   </div>
 </template>
 
@@ -32,21 +34,29 @@ import { mapGetters } from "vuex";
 export default {
   name: "fonts",
   mounted() {
-    this.getHeight();
+    for (let i = 0; i <= 12; i++) {
+      this.blocks.push("block");
+    }
   },
   updated() {
     this.getHeight();
   },
   methods: {
     getHeight() {
-      this.height = this.$refs.container.offsetHeight;
-      while (this.height > 128) {}
+      this.height_container = this.$refs.container.offsetHeight;
+      this.blocks.forEach(block => {
+        this.height_blocks[0].push(block);
+      });
+      if (this.height_container > 128) {
+        console.log(this.height_blocks);
+      }
     }
   },
   data() {
     return {
-      blocks: ["block"],
-      height: null
+      blocks: [],
+      height_container: null,
+      height_blocks: []
     };
   },
   computed: {
