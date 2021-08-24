115f7e96ec59999604dcb6482005fed1fb40a129
InSyn <Synergy95@yandex.ru>
2021-08-24 20:55:18 +0700

    Update fonts.vue

--- src/renderer/components/final_protocols/fonts.vue
+++ src/renderer/components/final_protocols/fonts.vue
@@ -1,14 +1,25 @@
 <template>
   <div
-    style="border-radius: 6px; width: 100%; height: 100%; padding: 16px; display:flex;flex-direction: column; justify-content:center;align-items: center"
+    style="position: relative;border-radius: 6px; width: 100%; height: 100%; padding: 16px; display:flex;flex-direction: column; justify-content:center;align-items: center"
     :style="{
       backgroundColor: $vuetify.theme.themes[appTheme].cardBackgroundRGBA
     }"
   >
     <div
+      v-if="loading"
+      style="position: absolute; display: flex;align-items: center;justify-content: center; height: 100%;width: 100%; z-index: 2"
+      :style="{
+        backgroundColor:
+          $vuetify.theme.themes[$store.getters['main/appTheme']]
+            .cardBackgroundRGBA
+      }"
+    >
+      <v-progress-circular indeterminate></v-progress-circular>
+    </div>
+    <div
       @click="console.log($refs)"
       ref="container"
-      style="border-radius: 4px; border: 1px solid #c3d9ff;max-height: 100%; overflow-y: auto;padding: 4px"
+      style="position: relative;padding: 4px;border-radius: 4px; border: 1px solid #c3d9ff;max-height: 100%; overflow-y: auto;"
     >
       <div v-for="(result, res_id) in results" :key="res_id">
         <v-row :ref="res_id" no-gutters>
@@ -37,6 +48,7 @@ export default {
     this.results = this.$store.getters["protocol_settings/testResults"];
     this.$nextTick(() => {
       let containerHeight = this.$refs["container"].offsetHeight;
+      let resultHeight = this.$refs[0][0].offsetHeight;
       let sumHeight = 0;
       for (let i in this.results) {
         sumHeight += this.$refs[i][0].offsetHeight;
@@ -44,14 +56,29 @@ export default {
       console.log(
         `${containerHeight} ${sumHeight} / ${Math.floor(
           sumHeight / containerHeight
-        )}`
+        )}, ${resultHeight}: ${Math.floor(containerHeight / resultHeight)}`
       );
+      setTimeout(() => {
+        let containerHeight = this.$refs["container"].offsetHeight;
+        let resultHeight = this.$refs[0][0].offsetHeight;
+        let sumHeight = 0;
+        for (let i in this.results) {
+          sumHeight += this.$refs[i][0].offsetHeight;
+        }
+        console.log(
+          `${containerHeight} ${sumHeight} / ${Math.floor(
+            sumHeight / containerHeight
+          )}, ${resultHeight}: ${Math.floor(containerHeight / resultHeight)}`
+        );
+        this.loading = false;
+      }, 768);
     });
   },
   updated() {},
   methods: {},
   data() {
     return {
+      loading: true,
       height: 0,
       results: []
     };
